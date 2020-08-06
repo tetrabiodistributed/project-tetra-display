854aaac55b6144e5c75e29ec11d1c69e4ca93deb
Christopher Smith <cbsmith@bernstein>
2020-08-05 18:34:45 -0700

    Clean up setup of pip/venv

--- setup.sh
+++ setup.sh
@@ -2,19 +2,16 @@
 
 HIGHLIGHT='\033[0;33m'
 NO_COLOR='\033[0m'
+VENV_NAME=venv
 
+VENV_BINDIR=${VENV_NAME}/bin
 
-if ! [ -d venv ]; then
-    python3 -m venv venv
+if ! [ -f ${VENV_BINDIR}/pip3 ]; then
+    python3 -m venv ${VENV_NAME}
 fi
 
-. venv/bin/activate
-
-if [ -f requirements.txt ]; then
-    pip3 install -r requirements.txt
-fi
-
-deactivate
+${VENV_BINDIR}/pip3 install pip --upgrade
+${VENV_BINDIR}/pip3 install -r requirements.txt
 
 if ! command -v docker &> /dev/null; then
     printf "\n${HIGHLIGHT}Please install Docker${NO_COLOR}"
