29dc9c5be09a33e06bdd93ea375f80378d8e9362
Fletcher Porter <me@fletcherporter.com>
2020-08-05 17:58:22 -0700

    Removed the print telling user to activate the venv, and just did it automatically in runtests.sh

--- runtests.sh
+++ runtests.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+. venv/bin/activate
 echo "Behaviour Tests:"
 behave
 echo $'\nUnit Tests:'
 python3 -m unittest
+deactivate
--- setup.sh
+++ setup.sh
@@ -19,6 +19,3 @@ deactivate
 if ! command -v docker &> /dev/null; then
     printf "\n${HIGHLIGHT}Please install Docker${NO_COLOR}"
 fi
-
-echo $'\nRun this to start the virtual environment'
-printf "${HIGHLIGHT}. venv/bin/activate${NO_COLOR}\n"
