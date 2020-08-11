20354678da345ad971f4960bc9075fa0a7f4508b
Fletcher Porter <me@fletcherporter.com>
2020-08-10 19:08:00 -0700

    Add ci for python unittests no docker (#24)
    * Removed the print telling user to activate the venv, and just did it automatically in runtests.sh
    
    * changed the cicd scheme to not run in a docker container because doing docker-in-docker stuff seems really not worth the effort
    
    * removed the redundant activate/deactivate steps in the cicd and fixed a syntax error
    
    * fixed error where python3-venv wasn't installed
    
    * fxed a typo in the cicd
    
    * added a badge to README for whether tests are passing
    
    * fixed a typo in the badge URL
    
    * Made the badge render as an image
    
    * changed the name of the tests workflow to be nicer in the badge
    
    * made runtests.sh exit non-zero on fail

--- runtests.sh
+++ runtests.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 
+. venv/bin/activate
 echo "Behaviour Tests:"
 behave
+behave_exit_code=$?
 echo $'\nUnit Tests:'
 python3 -m unittest
+unittest_exit_code=$?
+deactivate
+
+if [ $behave_exit_code -ne 0 ]; then
+    echo "behave tests failed!"
+fi
+if [ $unittest_exit_code -ne 0 ]; then
+    echo "unittests tests failed!"
+fi
+exit $(($behave_exit_code + $unittest_exit_code))
