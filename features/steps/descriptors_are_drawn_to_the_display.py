4ed6cfc351dd89e505129c046cbd4fd13b62f8f2
Fletcher Porter <me@fletcherporter.com>
2020-08-14 01:56:32 -0700

    removed unused imports

--- features/steps/descriptors_are_drawn_to_the_display.py
+++ features/steps/descriptors_are_drawn_to_the_display.py
@@ -1,14 +1,8 @@
-from behave import given, when, then, use_fixture
+from behave import given, when, then
 import json
-import os
 import math
 
-from selenium import webdriver
-import docker
-
 import constants
-import server
-from basic_websocket import ws_connect_retry
 
 
 @given("a file named {file} exists in {static_directory}")
