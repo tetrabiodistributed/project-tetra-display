6744e769cbbccde055ec40248bebf81b4e73ed4d
Fletcher Porter <me@fletcherporter.com>
2020-08-26 14:46:53 -0700

    added a move and link step to chromedriverinstall

--- chromedriverinstall.sh
+++ chromedriverinstall.sh
@@ -20,4 +20,8 @@ wget -N http://chromedriver.storage.googleapis.com/2.26/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
 chmod +x chromedriver
 
+sudo mv -f chromedriver /usr/local/share/chromedriver
+sudo ln -s /usr/local/share/chromedriver /usr/local/bin/chromedriver
+sudo ln -s /usr/local/share/chromedriver /usr/bin/chromedriver
+
 exit 0
