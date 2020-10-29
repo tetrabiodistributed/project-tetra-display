c8bb5a62974fbf4deb0f1762d7b56e31cd5bb08a
Fletcher Porter <me@fletcherporter.com>
2020-10-29 00:01:24 -0700

    added a timeout so the connection doesn't die

--- static/main.js
+++ static/main.js
@@ -1,26 +1,31 @@
-let socket = new WebSocket("ws://" + location.host + "/ws");
+setUpWebSocket();
 
-socket.onopen = function (event) {
-    console.log("websocket opened");
-    socket.send(JSON.stringify({"subscribe": ""}))
-};
+function setUpWebSocket() {
+    this.socket = new WebSocket("ws://" + location.host + "/ws");
 
-socket.onmessage = function (event) {
-    let data = JSON.parse(event.data)
-    console.log(data);
-    drawDataToPage(data)
-}
+    socket.onopen = function (event) {
+        console.log("websocket opened");
+        socket.send(JSON.stringify({"subscribe": ""}));
+    };
 
-socket.onclose = function (event) {
-    if (event.wasClean) {
-        console.log("connection closed");
-    } else {
-        console.log("connection died");
+    socket.onmessage = function (event) {
+        let data = JSON.parse(event.data)
+        console.log(data);
+        drawDataToPage(data);
     }
-}
 
-socket.onerror = function (event) {
-    console.log("error: " + event.message)
+    socket.onclose = function (event) {
+        setTimeout(setUpWebSocket, 5000);
+        if (event.wasClean) {
+            console.log("connection closed");
+        } else {
+            console.log("connection died");
+        }
+    }
+
+    socket.onerror = function (event) {
+        console.log("error: " + event.message)
+    }
 }
 
 function drawDataToPage(data) {
