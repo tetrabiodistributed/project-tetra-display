5c082a573047ce8009ffe82a5fed9129df69af45
Fletcher Porter <me@fletcherporter.com>
2021-01-05 21:19:36 -0800

    renamed constants in tidal_volume

--- tidal_volume.py
+++ tidal_volume.py
@@ -1,17 +1,17 @@
 import math
 
 from causal_integral_filter import CausalIntegralFilter
-import constants
+import tetra_constants
 
 
 class TidalVolume():
-
     def __init__(self, initial_time):
         self._integral_filter = CausalIntegralFilter(0, initial_time)
 
     def append(self, flow_rate, t):
         self._integral_filter.append(flow_rate, t)
-        if math.isclose(flow_rate, 0, abs_tol=constants.MINIMUM_TIDAL_VOLUME):
+        if math.isclose(flow_rate, 0,
+                        abs_tol=tetra_constants.MINIMUM_TIDAL_VOLUME):
             self._integral_filter.append_integral_value(0)
 
     def get_datum(self):
